7763c62d35f5abf13a5db20b89b9a10763f6b545
Damian Obajtek <damian.obajtek@gmail.com>
2019-09-06 17:07:19 +0200

    Add project files.

---
+++ Life/Life.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+
+namespace Life
+{
+    class Life
+    {
+       
+        public Life(int h, int w)
+        {
+            if (h <= 0 || w <= 0)
+                throw new ArgumentOutOfRangeException("Dimensions must be positive!");
+
+            Height = h;
+            Width = w;
+            Ticks = 0;
+            Grid = new bool[h, w];
+        }
+
+        public int Height { get; private set; }     
+        public int Width { get; private set; }      
+        public int Ticks { get; set; }             
+        private bool[,] Grid { get; set; }          
+
+        
+        public bool this[int y, int x]
+        {
+            get
+            {
+                if (IsOutOfBounds(y, x))
+                    throw new ArgumentOutOfRangeException("Coordinates must be non-negative!");
+
+                return Grid[y, x];
+            }
+
+            set
+            {
+                if (IsOutOfBounds(y, x))
+                    throw new ArgumentOutOfRangeException("Coordinates must be non-negative!");
+
+                Grid[y, x] = value;
+            }
+        }
+
+        public void Tick()
+        {
+            bool[,] clone = GetClonedBoard();
+
+            for (var j = 0; j < Height; ++j)
+                for (var i = 0; i < Width; ++i)
+                    clone[j, i] = GetNextState(j, i);
+
+            Grid = clone;
+            ++Ticks;
+        }
+
+        private bool IsOutOfBounds(int y, int x)
+        {
+            return y < 0 || y >= Height || x < 0 || x >= Width;
+        }
+
+        private int GetLiveNeighbours(int y, int x)
+        {
+            if (IsOutOfBounds(y, x))
+                throw new ArgumentOutOfRangeException("Coordinates must be non-negative!");
+
+            int count = 0;
+
+            for (var j = y - 1; j < y + 2; ++j)
+                for (var i = x - 1; i < x + 2; ++i)
+                    if (!IsOutOfBounds(j, i))
+                        count += Convert.ToInt32(Grid[j, i]);
+
+            return count - Convert.ToInt32(Grid[y, x]);
+        }
+
+        private bool GetNextState(int y, int x)
+        {
+            if (IsOutOfBounds(y, x))
+                throw new ArgumentOutOfRangeException("Coordinates must be non-negative!");
+
+            switch (GetLiveNeighbours(y, x))
+            {
+                case 2:
+                    return Grid[y, x];      
+                case 3:
+                    return true;            
+                default:
+                    return false;
+            }
+        }
+
+        private bool[,] GetClonedBoard()
+        {
+            bool[,] clone = new bool[Height, Width];
+
+            for (var j = 0; j < Height; ++j)
+                for (var i = 0; i < Width; ++i)
+                    clone[j, i] = Grid[j, i];
+
+            return clone;
+        }
+    }
+}
---
+++ Life/LifeUI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Life
+{
+    public partial class LifeUI : Form
+    {
+        private const int GRID_BUTTON_DIMENSION_PX = 25;
+        private const int GRID_DIMENSION_PX = 450;
+        private const int GRID_DIMENSION_CELLS = GRID_DIMENSION_PX / GRID_BUTTON_DIMENSION_PX;
+
+        private static readonly Color ALIVE_CELL_COLOUR = Color.White;
+        private static readonly Color DEAD_CELL_COLOUR = Color.DarkGray;
+
+        private Life engine = null;
+
+        public LifeUI()
+        {
+            InitializeComponent();
+            engine = new Life(GRID_DIMENSION_CELLS, GRID_DIMENSION_CELLS);
+        }
+      
+        private void LifeUI_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j + GRID_BUTTON_DIMENSION_PX <= GRID_DIMENSION_PX; j += GRID_BUTTON_DIMENSION_PX)
+                for (int i = 0; i + GRID_BUTTON_DIMENSION_PX <= GRID_DIMENSION_PX; i += GRID_BUTTON_DIMENSION_PX)
+                {
+                    Button newButton = new Button();
+                    newButton.Size = new Size(GRID_BUTTON_DIMENSION_PX, GRID_BUTTON_DIMENSION_PX);
+                    newButton.Location = new Point(i, j);
+                    newButton.Click += new EventHandler(ClickCell);
+                    gridUI.Controls.Add(newButton);
+                }
+
+            UpdateColours();
+        }
+       
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            engine.Tick();
+            generationUI.Text = engine.Ticks.ToString();
+            UpdateColours();
+        }
+       
+        private void startUI_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            startUI.Enabled = false;
+            stopUI.Enabled = true;
+        }
+        
+        private void stopUI_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            stopUI.Enabled = false;
+            startUI.Enabled = true;
+        }
+    
+        private void clearUI_Click(object sender, EventArgs e)
+        {           
+            timer.Enabled = false;
+            stopUI.Enabled = false;
+            startUI.Enabled = true;
+
+            engine = new Life(engine.Height, engine.Width);
+            generationUI.Text = engine.Ticks.ToString();
+
+            UpdateColours();
+        }
+        
+        private void ClickCell(object sender, EventArgs e)
+        {          
+            if (timer.Enabled)
+                return;
+         
+            int buttonLinearIndex = gridUI.Controls.IndexOf(sender as Control);
+            int y = buttonLinearIndex / engine.Width;
+            int x = buttonLinearIndex % engine.Width;
+                  
+            engine[y, x] = !engine[y, x];
+            ((Button)sender).BackColor =  engine[y, x] ? ALIVE_CELL_COLOUR : DEAD_CELL_COLOUR;
+        }
+       
+        private void UpdateColours()
+        {
+            for (int linearIndex = 0; linearIndex < gridUI.Controls.Count; ++linearIndex)
+                gridUI.Controls[linearIndex].BackColor =
+                    engine[linearIndex / engine.Width, linearIndex % engine.Width] ? ALIVE_CELL_COLOUR : DEAD_CELL_COLOUR;
+        }
+    }
+}
